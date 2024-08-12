227407d0fc007f82711d00b7aef07c94bfa0a675
darvin <todarvinrio@gmail.com>
2024-08-13 02:06:15 +0530

    add token contracts

--- src/processor.ts
+++ src/processor.ts
@@ -1,11 +1,64 @@
-import { Counter } from '@sentio/sdk'
-import { ERC20Processor } from '@sentio/sdk/eth/builtin'
+import { getPriceByType, token } from "@sentio/sdk/utils";
+import { BigDecimal, Counter, Gauge } from "@sentio/sdk";
 
-const tokenCounter = Counter.register('token')
+import {
+    CurveStableSwapNGContext,
+    CurveStableSwapNGProcessor,
+    AddLiquidityEvent,
+    RemoveLiquidityEvent,
+    TokenExchangeEvent,
+} from "./types/eth/curvestableswapng.js";
 
-const address = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
+import { RswETHContext } from "./types/eth/rsweth.js";
+import { MswETHContext } from "./types/eth/msweth.js";
 
-ERC20Processor.bind({ address }).onEventTransfer(async (event, ctx) => {
-  const val = event.args.value.scaleDown(18)
-  tokenCounter.add(ctx, val)
-})
+import { rswETH, mswETH, msw_rswETH_curve } from "./util.js";
+
+//////////////////////////////
+
+const rswETH_bal_acc = Counter.register("rswETH_bal");
+const mswETH_bal_acc = Counter.register("mswETH_bal");
+
+const addLiquidityHandler = async function (
+    event: AddLiquidityEvent,
+    ctx: CurveStableSwapNGContext
+) {
+    const mswETH_amt = event.args.token_amounts[0].scaleDown(18);
+    const rswETH_amt = event.args.token_amounts[1].scaleDown(18);
+
+    mswETH_bal_acc.add(ctx, mswETH_amt, { token: "mswETH" });
+    rswETH_bal_acc.add(ctx, rswETH_amt, { token: "mswETH" });
+};
+
+const removeLiquidityHandler = async function (
+    event: RemoveLiquidityEvent,
+    ctx: CurveStableSwapNGContext
+) {
+    const mswETH_amt = event.args.token_amounts[0].scaleDown(18);
+    const rswETH_amt = event.args.token_amounts[1].scaleDown(18);
+
+    mswETH_bal_acc.sub(ctx, mswETH_amt, { token: "mswETH" });
+    rswETH_bal_acc.sub(ctx, rswETH_amt, { token: "mswETH" });
+};
+
+const tokenExchangeHandler = async function (
+    event: TokenExchangeEvent,
+    ctx: CurveStableSwapNGContext
+) {
+    let mswETH_amt, rswETH_amt;
+    if (event.args.sold_id === BigInt(0)) {
+        mswETH_amt = event.args.tokens_sold.scaleDown(18);
+        rswETH_amt = event.args.tokens_bought.scaleDown(18);
+    } else {
+        mswETH_amt = event.args.tokens_bought.scaleDown(18);
+        rswETH_amt = event.args.tokens_sold.scaleDown(18);
+    }
+
+    mswETH_bal_acc.sub(ctx, mswETH_amt, { token: "mswETH" });
+    rswETH_bal_acc.sub(ctx, rswETH_amt, { token: "mswETH" });
+};
+
+CurveStableSwapNGProcessor.bind({ address: msw_rswETH_curve })
+    .onEventAddLiquidity(addLiquidityHandler)
+    .onEventRemoveLiquidity(removeLiquidityHandler)
+    .onEventTokenExchange(tokenExchangeHandler);
---
+++ src/util.ts
@@ -0,0 +1,8 @@
+import { EthChainId } from "@sentio/sdk/eth";
+
+
+export const rswETH = "0xFAe103DC9cf190eD75350761e95403b7b8aFa6c0";
+export const mswETH = "0x32bd822d615A3658A68b6fDD30c2fcb2C996D678";
+export const WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+
+export const msw_rswETH_curve = "0x84B5a3bD6acB304Cb89fFc43117A18BEDE062376";
